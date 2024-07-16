9afa7b0b36b7bf749f83526fbb8fc70b7ab691b2
Artur Melo <arturmelo@id.uff.br>
2024-07-16 15:16:18 -0300

    refactor: :fire: remove unused abilities

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -5,13 +5,7 @@ export const SAB = {};
  * @type {Object}
  */
 SAB.abilities = {
-  bdy: 'SAB.Ability.Body.long',
-  mdn: 'SAB.Ability.Mind.long',
-  lck: 'SAB.Ability.Luck.long',
 };
 
 SAB.abilityAbbreviations = {
-  bdy: 'SAB.Ability.Bdy.long',
-  mdn: 'SAB.Ability.Mnd.long',
-  lck: 'SAB.Ability.Lck.long',
 };
